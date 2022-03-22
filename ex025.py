450843f888a81f9f2ea6f385f5e60669abd6127b
rodten23 <rodten23@yahoo.com.br>
2022-03-22 19:30:41 -0300

    Exercício 25 melhorado.

--- ex025.py
+++ ex025.py
@@ -2,5 +2,11 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
 
 nome = str(input('Por favor, informe seu nome: ')).strip()
+# forma melhor
+if 'SILVA' in nome.upper():
+    print('Você faz parte da família Silva.')
+else:
+    print('Você não é da família Silva.')
+# forma mais simples
 print('Você faz parte da família Silva? {}'.format('SILVA' in nome.upper()))
 
